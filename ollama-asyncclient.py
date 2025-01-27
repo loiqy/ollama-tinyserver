f446804f60e5fb594e36d903b7d15efd3cd5eb83
Liqin Luo <1261760514@qq.com>
2025-01-27 18:51:36 +0000

    async client

---
+++ ollama-asyncclient.py
@@ -0,0 +1,120 @@
+import aiohttp
+import asyncio
+from typing import TypedDict, List, Any, TypeAlias
+from typing_extensions import NotRequired
+import requests
+
+class LLM_input(TypedDict):
+    system_prompt: NotRequired[str]
+    image: NotRequired[List[Any]]
+    history: NotRequired[List[Any]]
+    query : str
+
+LLM_output : TypeAlias = str
+    
+class LLM():
+    def __init__(self,
+                 model_name : str,
+                 api_keys : str|None,) -> None:
+        
+        self.model_name = model_name
+        self.api_keys = api_keys
+
+    def predict(self, input: LLM_input) -> LLM_output:
+        
+        response = self.API_server(input)
+        
+        return response
+
+    async def predict_async(self, input: LLM_input) -> LLM_output:
+        
+        response = await self.API_server_async(input)
+        
+        return response
+
+class OLLAMA(LLM):
+    def __init__(self, 
+                 model_name: str,
+                 api_keys : str|None,   # 实际上是url
+                 ) -> None:
+        
+        super().__init__(model_name, api_keys)
+
+    def API_server(self, input: LLM_input) -> LLM_output:
+        messages = self.messages(input)
+        
+        if input.get('response_format'):
+            raise ValueError("response_format is not supported in this model")
+        
+        data = {
+            "model": self.model_name,
+            "messages": messages,
+            "stream": False
+        }
+
+        response = requests.post(self.api_keys, json=data)
+        return response.json()['message']['content']
+
+    async def API_server_async(self, input: LLM_input) -> LLM_output:
+        messages = self.messages(input)
+
+        if input.get('response_format'):
+            raise ValueError("response_format is not supported in this model")
+        
+        data = {
+            "model": self.model_name,
+            "messages": messages,
+            "stream": False
+        }
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(self.api_keys, json=data) as response:
+                return await response.json()['message']['content']
+
+    def messages(self, input: LLM_input) -> List[dict]:
+        
+        messages = []
+        if input.get("system_prompt"):
+            messages.append({
+                "role": "system",
+                "content": input["system_prompt"]
+            })
+        content =[{"type": "text", "text": input["query"]}]
+        
+        if input.get('image'): #image should be a list of base64 encoded images
+            raise ValueError("image is not supported in this model")
+            image_file = [image for image in input.get('image')]
+            for image in image_file:
+                content.append(
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{image}"
+                            }
+                        }
+                    )
+        messages.append({"role": "user", "content": content})
+        
+        return messages
+    
+if __name__ == "__main__":
+    model_name = "deepseek-r1:32b"
+    api_keys = "http://localhost:11434/api/chat"
+    ollama = OLLAMA(model_name, api_keys)
+    inputs = [
+        {
+            "query": "What is the capital of France?"
+        },
+        {
+            "query": "What is the capital of Spain?"
+        },
+        {
+            "query": "What is the capital of Italy?"
+        }
+    ]
+    async def process_batch(batch):
+        tasks = [ollama.predict_async(i) for i in batch]
+        return await asyncio.gather(*tasks)
+    responses = asyncio.run(process_batch(inputs))
+    for response in responses:
+        print(response)
